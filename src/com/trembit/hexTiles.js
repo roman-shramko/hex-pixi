b9addbd63558cb876c9ffaf8eb53abc7084841eb
AndyGura <andy.gura.jd@gmail.com>
2017-04-06 03:34:44 +0300

    changed background; implemented putting hexagon markers as vector on the right coordinates given the cell coordinates

--- src/com/trembit/hexTiles.js
+++ src/com/trembit/hexTiles.js
@@ -1,3 +1,15 @@
+const TILE_SCALE_MULTIPLIER = 0.064941;
+const TILE_SCALE_ASPECT_FIX_MULTIPLIER = 1.150055;
+
+const HEXAGON_BIG_DIAMETER = 86;
+const HEXAGON_SMALL_DIAMETER = Math.sqrt(3)*HEXAGON_BIG_DIAMETER/2;
+const HEXAGONS_LAYOUT_GAP = 2;
+
+const HEXAGON_IN_TILE_OFFSET = {x: 21, y: 0};
+
+const HEX_ANGLES = [0, Math.PI/3, 2*Math.PI/3, Math.PI, -2*Math.PI/3, -Math.PI/3];
+
+
 var onWindowResize = function() {
     renderer.resize(window.innerWidth, window.innerHeight);
     tilingSprite.width = window.innerWidth;
@@ -24,9 +36,13 @@ var tilingSprite = new PIXI.extras.TilingSprite(
     window.innerWidth,
     window.innerHeight
 );
+tilingSprite.tileScale.x = zoomScale*TILE_SCALE_MULTIPLIER;
+tilingSprite.tileScale.y = zoomScale*TILE_SCALE_MULTIPLIER*TILE_SCALE_ASPECT_FIX_MULTIPLIER;
 stage.addChild(tilingSprite);
 
 render();
+
+var markers = [];
 
 function render() {
     // render the root container
@@ -35,14 +51,16 @@ function render() {
 }
 
 addDragNDrop();
+addElementsLogic();
 function addDragNDrop() {
     stage.interactive = true;
 
     var isDragging = false,
-        prevX, prevY;
+        prevX, prevY, downX, downY;
 
     stage.mousedown = function (moveData) {
         var pos = moveData.data.global;
+        downX = pos.x; downY = pos.y;
         prevX = pos.x; prevY = pos.y;
         isDragging = true;
     };
@@ -57,11 +75,21 @@ function addDragNDrop() {
 
         tilingSprite.tilePosition.x += dx;
         tilingSprite.tilePosition.y += dy;
+        for (var i=0;i<markers.length;i++) {
+            markers[i].position.x += dx;
+            markers[i].position.y += dy;
+        }
         prevX = pos.x; prevY = pos.y;
     };
 
-    stage.mouseup = function (moveDate) {
+    stage.mouseup = function (moveData) {
         isDragging = false;
+        var pos = moveData.data.global;
+        var dx = pos.x - downX;
+        var dy = pos.y - downY;
+        if (stage.mouseclick && dx*dx+dy*dy < 10) {
+            stage.mouseclick(moveData);
+        }
     };
 
     var body = document.body;
@@ -83,7 +111,7 @@ function addDragNDrop() {
         // Find the direction that was scrolled
         var direction = wheelDirection(evt);
         // Set the old scale to be referenced later
-        var oldScale = tilingSprite.tileScale.x;
+        var oldScale = tilingSprite.tileScale.x/TILE_SCALE_MULTIPLIER;
         // Manipulate the scale based on direction
         zoomScale = oldScale + direction;
         //Check to see that the scale is not outside of the specified bounds
@@ -94,7 +122,58 @@ function addDragNDrop() {
         var mouseY = evt.clientY;
         tilingSprite.tilePosition.x = (tilingSprite.tilePosition.x - mouseX) * (zoomScale / oldScale) + mouseX;
         tilingSprite.tilePosition.y = (tilingSprite.tilePosition.y - mouseY) * (zoomScale / oldScale) + mouseY;
-        tilingSprite.tileScale.x = zoomScale;
-        tilingSprite.tileScale.y = zoomScale;
+        tilingSprite.tileScale.x = zoomScale*TILE_SCALE_MULTIPLIER;
+        tilingSprite.tileScale.y = zoomScale*TILE_SCALE_MULTIPLIER*TILE_SCALE_ASPECT_FIX_MULTIPLIER;
+        for (var i=0;i<markers.length;i++) {
+            markers[i].position.x = (markers[i].position.x - mouseX) * (zoomScale / oldScale) + mouseX;
+            markers[i].position.y = (markers[i].position.y - mouseY) * (zoomScale / oldScale) + mouseY;
+            markers[i].scale.x = zoomScale;
+            markers[i].scale.y = zoomScale;
+        }
     }
+}
+
+function addElementsLogic() {
+    stage.mouseclick = function (moveData) {
+        for (var i = 0;i< 50; i++) {
+            for (var j = 0;j< 50; j++) {
+                addMarker(i, j);
+            }
+        }
+    };
+
+    function addMarker(i, j) {
+        //calculate native coords
+        var x = i * (Math.sqrt(Math.pow(HEXAGON_SMALL_DIAMETER,2)-Math.pow(HEXAGON_SMALL_DIAMETER/2,2)) + HEXAGONS_LAYOUT_GAP);
+        var y = (i % 2 == 1) ? (j-0.5) * (HEXAGON_SMALL_DIAMETER+HEXAGONS_LAYOUT_GAP) : j * (HEXAGON_SMALL_DIAMETER+HEXAGONS_LAYOUT_GAP);
+        //adjust position to background texture
+        x += HEXAGON_IN_TILE_OFFSET.x;
+        y += HEXAGON_IN_TILE_OFFSET.y;
+        //calculate real screen coords
+        x = x*zoomScale + tilingSprite.tilePosition.x;
+        y = y*zoomScale + tilingSprite.tilePosition.y;
+        var hexSprite = createHexagon(HEXAGON_BIG_DIAMETER/2, 0xaa3333);
+        hexSprite.position.x = x;
+        hexSprite.position.y = y;
+        hexSprite.scale.x = zoomScale;
+        hexSprite.scale.y = zoomScale;
+        stage.addChild(hexSprite);
+        markers.push(hexSprite);
+    }
+
+    function createHexagon(bigRadius, color) {
+        var g = new PIXI.Graphics();
+        g.beginFill(color);
+        var polygonArgs = [];
+        for (var i=0; i<HEX_ANGLES.length; i++) {
+            polygonArgs.push(bigRadius*Math.cos(HEX_ANGLES[i]));
+            polygonArgs.push(bigRadius*Math.sin(HEX_ANGLES[i]));
+        }
+        polygonArgs.push(bigRadius*Math.cos(HEX_ANGLES[0]));
+        polygonArgs.push(bigRadius*Math.sin(HEX_ANGLES[0]));
+        g.drawPolygon(polygonArgs);
+        g.endFill();
+        return g;
+    }
+
 }
